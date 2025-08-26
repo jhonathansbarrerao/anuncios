316b526cc2a4f92b9bb40c3bf882a51174266c95
jsbo <jhonathans.barrerao@unilibre.edu.co>
2025-08-25 19:37:11 -0500

    vista detalle anuncio

--- src/anuncios/run.py
+++ src/anuncios/run.py
@@ -6,3 +6,7 @@ anuncios = []
 @app.route("/")
 def index():
     return f"En home hay {len(anuncios)} anuncios"
+
+@app.route("/ad/<string:slug>/")
+def show_ad(slug):
+    return f"Mostrando el anuncio {slug}"
