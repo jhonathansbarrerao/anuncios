bc912aa6664eef50affe6639a1d0994ad185d111
jsbo <jhonathans.barrerao@unilibre.edu.co>
2025-08-26 13:30:03 -0500

    procesamiento de formulario

--- src/anuncios/run.py
+++ src/anuncios/run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
@@ -18,4 +18,13 @@ def ad_form(ad_id=None):
 
 @app.route("/signup/" methods=["GET", "POST"])
 def show_signup_form():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        next = request.args.get('next', None)
+        if next:
+            return redirect(next)
+        return redirect(url_for('index'))
     return render_template("signup_form.html")
+
