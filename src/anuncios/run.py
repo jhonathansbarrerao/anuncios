45f7578dbe342c43ce8173063430d280d51ef2d6
jsbo <jhonathans.barrerao@unilibre.edu.co>
2025-08-28 20:32:11 -0500

    vista crear/modificar anuncio

--- src/anuncios/run.py
+++ src/anuncios/run.py
@@ -11,3 +11,8 @@ def index():
 @app.route("/ad/<string:slug>/")
 def show_post(slug):
     return f"Mostrando el anuncio {slug}"
+
+@app.route("/admin/ad/")
+@app.route("/admin/ad/<int:ad_id>/")
+def ad_form(ad_id=None):
+    return f"Mostrando el formulario de anuncio {ad_id}"
