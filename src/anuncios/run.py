b5a8eea1386f3b904a19a563822e786f982ba4df
jsbo <jhonathans.barrerao@unilibre.edu.co>
2025-08-28 20:26:25 -0500

    vista home

--- src/anuncios/run.py
+++ src/anuncios/run.py
@@ -2,6 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+ads = []
+
+@app.route("/")
+def index():
+    return f"{len(ads)} ads"
