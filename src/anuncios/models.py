9e64d31191cc9a568982025ae2cc403750cae3d5
jsbo <jhonathans.barrerao@unilibre.edu.co>
2025-09-08 18:30:27 -0500

    uso de contraseña

--- src/anuncios/models.py
+++ src/anuncios/models.py
@@ -7,8 +7,14 @@ class User(UserMixin):
         self.id = id
         self.name = name
         self.email = email
-        self.password = password
+        self.password = generate_password_hash(password)
         self.is_admin = is_admin
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.email)
