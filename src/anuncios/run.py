585aa59846eb59ab925d47919d2e9d6848fc12a6
jsbo <jhonathans.barrerao@unilibre.edu.co>
2025-10-09 21:00:27 -0500

    actualizando credeciales de la base de datos

--- src/anuncios/run.py
+++ src/anuncios/run.py
@@ -6,7 +6,7 @@ from urllib.parse import urlparse
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ba2e18ce248bab7ce9425333f0420b57a5f07dfef342e1876d3013a524acf416f813af3071a65e3860475fe8e81c3a42c3c8fa65051de39aa2037fa695b305a7bc7044a415eb'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg://postgres:testing@localhost:5432/anuncios'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg://jhbarrera:class2025@localhost:5432/anuncios'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 login_manager = LoginManager(app)
