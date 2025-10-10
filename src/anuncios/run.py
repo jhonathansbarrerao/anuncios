3d8a7ca355ae0e97f705f7933f5513e19486b371
jsbo <jhonathans.barrerao@unilibre.edu.co>
2025-10-09 20:10:57 -0500

    agrega configuracion inicial base de datos

--- src/anuncios/run.py
+++ src/anuncios/run.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_login import LoginManager, current_user, login_user, logout_user, login_required
+from flask_sqlalchemy import SQLAlchemy
 from forms import SignupForm, PostForm, LoginForm
 from urllib.parse import urlparse
 from models import users, get_user, User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ba2e18ce248bab7ce9425333f0420b57a5f07dfef342e1876d3013a524acf416f813af3071a65e3860475fe8e81c3a42c3c8fa65051de39aa2037fa695b305a7bc7044a415eb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg://postgres:testing@localhost:5432/anuncios'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
+db = SQLAlchemy(app)
 
 ads = []
 
